5a46a55709e4c9f47596d080328f6b9649969464
FranCali <fr.califano@gmail.com>
2019-12-13 16:10:29 +0100

    maven config

---
+++ src/main/java/it/unisa/studenti/App.java
@@ -0,0 +1,13 @@
+package it.unisa.studenti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
